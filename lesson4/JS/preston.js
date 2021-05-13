b133f3026f0c91596bc2fb0159285e7febd6fb21
denislazo1610 <77086455+denislazo1610@users.noreply.github.com>
2021-05-12 20:50:46 -0600

    changes

--- lesson4/JS/preston.js
+++ lesson4/JS/preston.js
@@ -1,8 +1,9 @@
+const navi = document.querySelector(".navigation");
 
-const navi = document.querySelector('.navigation');
-
-const boton = document.querySelector('.boton');
+const boton = document.querySelector(".boton");
 
 function toggleMenu() {
-    document.getElementsByClassName("navigation")[0].classList.toggle("responsive";
+  document
+    .getElementsByClassName("navigation")[0]
+    .classList.toggle("responsive");
 }
