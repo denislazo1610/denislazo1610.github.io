3d5ae5f5775cb44f9ac21e679f4eae909f9f9eb3
denislazo1610 <77086455+denislazo1610@users.noreply.github.com>
2021-05-20 13:51:39 -0600

    changes

---
+++ lesson5/JS/bom.js
@@ -0,0 +1,4 @@
+// document.querySelector("#favchap").value = "Nooo";
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+// const button = document.querySelector("");
