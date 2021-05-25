0dee2e1640a85f19d491b726562b3b1dddc2fde4
denislazo1610 <77086455+denislazo1610@users.noreply.github.com>
2021-05-25 13:06:54 -0600

    changes

--- lesson5/JS/preston.js
+++ lesson5/JS/preston.js
@@ -16,28 +16,28 @@ let year = new Date().getFullYear();
 
 document.querySelector("#year").textContent = year;
 
-console.log(document.querySelector(".grid-container .forecast .first_day"));
-
-console.log(document.querySelector(".grid-container .forecast .second_day"));
-
 // This is going to tell days
 var date = new Date();
 let first_day = new Date().toLocaleString("en-us", { weekday: "long" });
+document.querySelector(".grid-container .forecast .first_day").textContent =
+  first_day;
 
 date.setDate(new Date().getDate() + 1);
 let second_day = date.toLocaleString("en-us", { weekday: "long" });
+document.querySelector(".grid-container .forecast .second_day").textContent =
+  second_day;
 
 date.setDate(new Date().getDate() + 2);
 let third_day = date.toLocaleString("en-us", { weekday: "long" });
+document.querySelector(".grid-container .forecast .third_day").textContent =
+  third_day;
 
 date.setDate(new Date().getDate() + 3);
 let fourth_day = date.toLocaleString("en-us", { weekday: "long" });
+document.querySelector(".grid-container .forecast .fourth_day").textContent =
+  fourth_day;
 
 date.setDate(new Date().getDate() + 4);
 let fifth_day = date.toLocaleString("en-us", { weekday: "long" });
-
-console.log(first_day);
-console.log(second_day);
-console.log(third_day);
-console.log(fourth_day);
-console.log(fifth_day);
+document.querySelector(".grid-container .forecast .fifth_day").textContent =
+  fifth_day;
