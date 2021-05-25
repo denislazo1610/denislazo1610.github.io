76ffebae9c1e61ec2f3bc3b6f7d92dcd18e19d7e
denislazo1610 <77086455+denislazo1610@users.noreply.github.com>
2021-05-25 12:58:56 -0600

    changes

--- lesson5/JS/preston.js
+++ lesson5/JS/preston.js
@@ -15,3 +15,17 @@ document.querySelector("#lastUpdated").textContent = lastUpdated;
 let year = new Date().getFullYear();
 
 document.querySelector("#year").textContent = year;
+
+console.log(document.querySelector(".grid-container .forecast .first_day"));
+
+console.log(document.querySelector(".grid-container .forecast .second_day"));
+
+// This is going to tell what day is today
+let first_day = new Date().toLocaleString("en-us", { weekday: "long" });
+
+var tomorrow = new Date();
+tomorrow.setDate(new Date().getDate() + 1);
+let second_day = tomorrow.toLocaleString("en-us", { weekday: "long" });
+
+console.log(date);
+console.log(second_day);
