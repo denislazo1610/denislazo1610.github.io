a27253d71546ae134ed4de347dc61aa44ec69053
denislazo1610 <77086455+denislazo1610@users.noreply.github.com>
2021-06-02 20:37:16 -0600

    changes

--- lesson6/JS/windchill.js
+++ lesson6/JS/windchill.js
@@ -16,7 +16,7 @@ function windchill(t, s) {
     0.6215 * t -
     35.75 * Math.pow(s, 0.16) +
     0.4275 * t * Math.pow(s, 0.16);
-  return respuesta;
+  return Math.round(respuesta);
 }
 
 // console.log(windchill(3, 4));
