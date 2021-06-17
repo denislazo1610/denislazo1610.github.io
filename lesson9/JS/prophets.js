619bba93ad51cd960499b2d8d071f10eaf38a6b6
denislazo1610 <77086455+denislazo1610@users.noreply.github.com>
2021-06-16 21:42:17 -0600

    changes

---
+++ lesson9/JS/prophets.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const requestURL =
+  "https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json";
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject); // temporary checking for valid response and data parsing
+    const prophets = jsonObject["prophets"];
+
+    for (let i = 0; i < prophets.length; i++) {
+      let card = document.createElement("section");
+      let h2 = document.createElement("h2");
+      let birthday = document.createElement("h3");
+      let placeBirth = document.createElement("h3");
+      let image = document.createElement("img");
+
+      h2.textContent = prophets[i].name + " " + prophets[i].lastname;
+      birthday.textContent = "Date of Birth: " + prophets[i].birthdate;
+      placeBirth.textContent = "Place of Birth: " + prophets[i].placeBirth;
+      image.setAttribute("src", prophets[i].imageurl);
+
+      card.appendChild(h2);
+      card.appendChild(birthday);
+      card.appendChild(placeBirth);
+      card.appendChild(image);
+
+      document.querySelector("div.cards").appendChild(card);
+    }
+  });
