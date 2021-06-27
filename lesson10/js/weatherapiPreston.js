cf249bcb7cf501c31b99a0732bb36272017e71af
denislazo1610 <77086455+denislazo1610@users.noreply.github.com>
2021-06-26 21:25:28 -0600

    changhes

---
+++ lesson10/js/weatherapiPreston.js
@@ -0,0 +1,18 @@
+const idCity = '5604473';
+const APPID = '998a024b00b4d5372043d31a8e17db75';
+
+const apiURL =
+  `https://api.openweathermap.org/data/2.5/weather?id=${idCity}&appid=${APPID}&units=imperial`;
+  // `https://api.openweathermap.org/data/2.5/forecast?id=${idCity}&appid=${APPID}&units=imperial`;
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+const desc = jsObject.weather[0].description;  // note how we reference the weather array
+document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+document.getElementById('icon').setAttribute('alt', desc);
+  });
