4d29ed8ecf68adf7be959e110039eab3a67f9f2c
denislazo1610 <77086455+denislazo1610@users.noreply.github.com>
2021-06-26 21:38:19 -0600

    changes

--- lesson10/js/weatherapiPreston.js
+++ lesson10/js/weatherapiPreston.js
@@ -9,10 +9,13 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    document.getElementById('current-temp').textContent = jsObject.main.temp;
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-const desc = jsObject.weather[0].description;  // note how we reference the weather array
-document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-document.getElementById('icon').setAttribute('alt', desc);
+    document.querySelector('.currently').textContent = jsObject.main.temp;
+    document.querySelector('.high').textContent = jsObject.main.temp_max;
+    document.querySelector('.humidity').textContent = jsObject.main.humidity;
+    document.querySelector('.wind-speed').textContent = jsObject.wind.speed;
+//     const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+// const desc = jsObject.weather[0].description;  // note how we reference the weather array
+// document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+// document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+// document.getElementById('icon').setAttribute('alt', desc);
   });
