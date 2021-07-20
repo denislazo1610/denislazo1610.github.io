e95d887a532e247897d05482c2f2f3101ed16b64
denislazo1610 <77086455+denislazo1610@users.noreply.github.com>
2021-07-20 00:33:57 -0600

    changes

---
+++ lesson12/js/forecast.js
@@ -0,0 +1,18 @@
+// const APPID = "998a024b00b4d5372043d31a8e17db75";
+const APPIDForecast = "d0fb9a57fc049f4f46ae9bff538ac131";
+
+var idCity = "3941584";
+
+// const apiURL = `https://api.openweathermap.org/data/2.5/weather?id=${idCity}&appid=${APPID}&units=imperial`;
+const apiURLForecast = `https://api.openweathermap.org/data/2.5/forecast?id=${idCity}&appid=${APPIDForecast}&units=imperial`;
+
+fetch(apiURLForecast)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+    for (var i = 0; i < 3; i++) {
+      document.querySelector(`.Cusco${i}Day`).textContent =
+        jsObject.list[i].main.temp;
+    }
+  });
