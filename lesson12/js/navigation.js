486553faf9645a43695929c7ca3b8ab15e78fd8e
denislazo1610 <77086455+denislazo1610@users.noreply.github.com>
2021-07-21 04:21:16 -0600

    changes

---
+++ lesson12/js/navigation.js
@@ -0,0 +1,9 @@
+const navi = document.querySelector(".navigation");
+
+const boton = document.querySelector(".boton");
+
+function toggleMenu() {
+  document
+    .getElementsByClassName("navigation")[0]
+    .classList.toggle("responsive");
+}
