5caad20d5fc6ba2e00b21424ac2e144812ae61e0
denislazo1610 <77086455+denislazo1610@users.noreply.github.com>
2021-07-21 03:32:48 -0600

    changes

--- lesson12/js/directory.js
+++ lesson12/js/directory.js
@@ -12,31 +12,78 @@ fetch("json/directory.json")
       console.log("grid");
 
       document.querySelector(".contentDirectory").style.display = "grid";
+      document.querySelector(".contentDirectory").style.gridTemplateColumns =
+        "1fr 1fr 1fr";
+
+      for (let i = 0; i < businesses.length; i++) {
+        let card = document.createElement("section");
+        let name = document.createElement("h1");
+        let contact = document.createElement("p");
+        let logo = document.createElement("img");
+        let link = document.createElement("a");
+
+        logo.src = businesses[i].logo;
+        name.textContent = businesses[i].name;
+        link.textContent = businesses[i].link;
+        contact.textContent = businesses[i].contact;
+        // button.setAttribute("href", "join.html");
+
+        card.appendChild(name);
+        card.appendChild(contact);
+        card.appendChild(logo);
+        card.appendChild(link);
+
+        document.querySelector(".contentDirectory").appendChild(card);
+      }
     });
 
     document.querySelector(".listView").addEventListener("click", function () {
       console.log("list");
+
+      document.querySelector(".contentDirectory").style.display = "grid";
+      document.querySelector(".contentDirectory").style.gridTemplateColumns =
+        "1fr";
+
+      for (let i = 0; i < businesses.length; i++) {
+        let card = document.createElement("section");
+        let name = document.createElement("h1");
+        let contact = document.createElement("p");
+        let logo = document.createElement("img");
+        let link = document.createElement("a");
+
+        logo.src = businesses[i].logo;
+        name.textContent = businesses[i].name;
+        link.textContent = businesses[i].link;
+        contact.textContent = businesses[i].contact;
+        // button.setAttribute("href", "join.html");
+
+        card.appendChild(name);
+        card.appendChild(contact);
+        card.appendChild(logo);
+        card.appendChild(link);
+
+        document.querySelector(".contentDirectory").appendChild(card);
+      }
     });
 
-    for (let i = 0; i < businesses.length; i++) {
-      let card = document.createElement("section");
-      let name = document.createElement("h1");
-      let contact = document.createElement("p");
-      let logo = document.createElement("img");
-      let link = document.createElement("a");
+    // for (let i = 0; i < businesses.length; i++) {
+    //   let card = document.createElement("section");
+    //   let name = document.createElement("h1");
+    //   let contact = document.createElement("p");
+    //   let logo = document.createElement("img");
+    //   let link = document.createElement("a");
 
-      logo.src = businesses[i].logo;
-      name.textContent = businesses[i].name;
-      link.textContent = businesses[i].link;
-      contact.textContent = businesses[i].contact;
-      // button.setAttribute("href", "join.html");
+    //   logo.src = businesses[i].logo;
+    //   name.textContent = businesses[i].name;
+    //   link.textContent = businesses[i].link;
+    //   contact.textContent = businesses[i].contact;
+    //   // button.setAttribute("href", "join.html");
 
-      //   card.appendChild(image);
-      //   card.appendChild(content);
-      //   content.appendChild(name);
-      //   content.appendChild(summary);
-      //   content.appendChild(button);
+    //   card.appendChild(name);
+    //   card.appendChild(contact);
+    //   card.appendChild(logo);
+    //   card.appendChild(link);
 
-      //   document.querySelector(".business_info").appendChild(card);
-    }
+    //   document.querySelector(".contentDirectory").appendChild(card);
+    // }
   });
