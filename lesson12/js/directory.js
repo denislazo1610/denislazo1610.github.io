de20c8419d5768121487406b913e291c33e2fc6f
denislazo1610 <77086455+denislazo1610@users.noreply.github.com>
2021-07-21 03:27:30 -0600

    changes

--- lesson12/js/directory.js
+++ lesson12/js/directory.js
@@ -8,32 +8,35 @@ fetch("json/directory.json")
     console.table(jsonObject); // temporary checking for valid response and data parsing
     const businesses = jsonObject["businesses"];
 
+    document.querySelector(".gridView").addEventListener("click", function () {
+      console.log("grid");
+
+      document.querySelector(".contentDirectory").style.display = "grid";
+    });
+
+    document.querySelector(".listView").addEventListener("click", function () {
+      console.log("list");
+    });
+
     for (let i = 0; i < businesses.length; i++) {
-      document
-        .querySelector(".gridView")
-        .addEventListener("click", function () {
-          console.log("NOOO");
-        });
-
       let card = document.createElement("section");
-      let content = document.createElement("article");
       let name = document.createElement("h1");
-      let summary = document.createElement("p");
-      let image = document.createElement("img");
-      let button = document.createElement("a");
+      let contact = document.createElement("p");
+      let logo = document.createElement("img");
+      let link = document.createElement("a");
 
-      image.src = businesses[i].image;
+      logo.src = businesses[i].logo;
       name.textContent = businesses[i].name;
-      button.textContent = businesses[i].button;
-      summary.textContent = businesses[i].summary;
-      button.setAttribute("href", "join.html");
+      link.textContent = businesses[i].link;
+      contact.textContent = businesses[i].contact;
+      // button.setAttribute("href", "join.html");
 
-      card.appendChild(image);
-      card.appendChild(content);
-      content.appendChild(name);
-      content.appendChild(summary);
-      content.appendChild(button);
+      //   card.appendChild(image);
+      //   card.appendChild(content);
+      //   content.appendChild(name);
+      //   content.appendChild(summary);
+      //   content.appendChild(button);
 
-      document.querySelector(".business_info").appendChild(card);
+      //   document.querySelector(".business_info").appendChild(card);
     }
   });
